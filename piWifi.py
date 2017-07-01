0a0cad837fd57326fce1edf91a5ebcd30460a85c
Paul Lassen <paullassen+github@protonmail.com>
2017-07-01 00:38:36 +0000

    Fixed indentation

--- piWifi.py
+++ piWifi.py
@@ -7,8 +7,10 @@ netList = sorted(networks, key=lambda x: x.signal, reverse=True)
 netDat = {}
 for net in netList:
     if not net.ssid == '':
-		netDat[net.ssid] = net.signal
+	netDat[net.ssid] = net.signal
         print(net.signal),
         print('\t'),
         print(net.ssid)
+print('')
+print(netDat)
 r = requests.post("http://5b4b7e6f.ngrok.io", data=netDat)
