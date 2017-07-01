6b3d7091670ec8028c663abe0eadf0785f420b99
paullassen <paullassen+github@protonmail.com>
2017-06-30 17:54:06 -0700

    added piId to post request

--- piWifi.py
+++ piWifi.py
@@ -1,10 +1,25 @@
 from wifi import Cell
 import requests
 
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+
+  return cpuserial
+
 networks = Cell.all('wlan0')
 netList = sorted(networks, key=lambda x: x.signal, reverse=True)
 
 netDat = {}
+netDat["RasPId"] = getserial()
 for net in netList:
     if not net.ssid == '':
 	netDat[net.ssid] = net.signal
